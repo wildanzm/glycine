8e316221c23822aed9f42aca000047423d8a799e
root <root@server.soysmart360.cloud>
2025-09-17 15:13:11 +0700

    fix: dashboard and production

--- glycine/settings/production.py
+++ glycine/settings/production.py
@@ -4,3 +4,5 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['154.19.37.54', 'soysmart360.cloud', 'www.soysmart360.cloud']
+
+CSRF_TRUSTED_ORIGINS = ['https://soysmart360.cloud', 'https://www.soysmart360.cloud']
