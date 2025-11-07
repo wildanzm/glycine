7e2a02bd11361239b2b58eb227eef73eca180de6
wildanzm <wildanmanapi123@gmail.com>
2025-11-07 11:21:25 +0700

    fix: remove redundant security settings in production configuration

--- glycine/settings/production.py
+++ glycine/settings/production.py
@@ -7,14 +7,6 @@ ALLOWED_HOSTS = ['154.19.37.54', 'soysmart360.cloud', 'www.soysmart360.cloud']
 
 CSRF_TRUSTED_ORIGINS = ['https://soysmart360.cloud', 'https://www.soysmart360.cloud']
 
-# Security
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_SECONDS = 3600
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
