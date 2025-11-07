7b30018aeccf5144b7f0d03ce72be966c1f5f3e2
wildanzm <wildanmanapi123@gmail.com>
2025-11-07 11:01:47 +0700

    fix: ensure CSRF_TRUSTED_ORIGINS and STATIC_ROOT are correctly defined in production settings

--- glycine/settings/production.py
+++ glycine/settings/production.py
@@ -6,3 +6,5 @@ DEBUG = False
 ALLOWED_HOSTS = ['154.19.37.54', 'soysmart360.cloud', 'www.soysmart360.cloud']
 
 CSRF_TRUSTED_ORIGINS = ['https://soysmart360.cloud', 'https://www.soysmart360.cloud']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
