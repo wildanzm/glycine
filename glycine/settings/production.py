44401adf3833ea0e314bbfe07ea15ceca00048af
wildanzm <wildanmanapi123@gmail.com>
2025-11-07 11:04:42 +0700

    fix: add STATICFILES_STORAGE configuration for production settings

--- glycine/settings/production.py
+++ glycine/settings/production.py
@@ -7,4 +7,6 @@ ALLOWED_HOSTS = ['154.19.37.54', 'soysmart360.cloud', 'www.soysmart360.cloud']
 
 CSRF_TRUSTED_ORIGINS = ['https://soysmart360.cloud', 'https://www.soysmart360.cloud']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
